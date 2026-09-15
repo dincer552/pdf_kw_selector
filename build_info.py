ef799eda66311963fe509f58fca07503be5a25b3
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-09-15 15:16:34 +0300

    Install VOCLEAN project selector hook

--- build_info.py
+++ build_info.py
@@ -6,6 +6,7 @@ import pdf_hover_indicator  # noqa: F401
 import status_badges  # noqa: F401
 import button_label_fix  # noqa: F401
 import ui_polish  # noqa: F401
+import voclean_project_selector  # noqa: F401
 
 BUILD_SHA = "development"
 BUILD_VERSION = "v0.5.4"
